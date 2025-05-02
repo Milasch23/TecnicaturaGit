1f077a17d0e5ee2dcd95a111f909acea09b73f10
Milasch23 <kamyxsch@gmail.com>
2025-05-02 16:41:39 -0300

    Tipos de Variables y memoria - Clase 2

---
+++ Python/act_clase2.py
@@ -0,0 +1,13 @@
+miVariable = 6
+print(miVariable)
+miVariable = "Hola, ¿Qué tal? ¿Todo bien?"
+print(miVariable)
+miVariable = 7.2
+print(miVariable)
+x = 9.2
+y = 8
+z = x + y
+print(id(x))
+# Así se escribe un comentario
+print(id(y))
+print(id(z))

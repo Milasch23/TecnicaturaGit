dcf8cf486b7a20749ade1d81cea39ed63a4843c4
Milasch23 <kamyxsch@gmail.com>
2025-05-03 18:04:57 -0300

    Hola Mundo en Javascript - Clase 2

---
+++ Javascript/01_01_holaMundo.js
@@ -0,0 +1,8 @@
+/*Videos*/
+
+var nombre = "Camila";
+var nombre = "Camila \nFlorencia\n";
+var apellido = "Scheurer";
+console.log(nombre + apellido);
+var saludo = "Hola Mundo desde Javascript";
+console.log(saludo);

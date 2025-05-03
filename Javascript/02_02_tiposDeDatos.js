9ae4d31b6f1a9be321c60ca5f4d0217bc3021551
Milasch23 <kamyxsch@gmail.com>
2025-05-03 18:27:09 -0300

    Tipos de Datos - Clase 3

---
+++ Javascript/02_02_tiposDeDatos.js
@@ -0,0 +1,15 @@
+// Tipos de Datos en Javascript
+
+var nombre = "Camila"; //String
+console.log(nombre);
+
+var num = 85;
+console.log(num); //Numérico
+
+var objeto = {
+    nombre: "Camila",
+    apellido: "Scheurer",
+    tel: 123456789
+}
+console.log(objeto); //Objeto
+

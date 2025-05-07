bb6b82e74e48ee2c64f34a90f66eeadf3c55bc76
Milasch23 <kamyxsch@gmail.com>
2025-05-06 22:00:09 -0300

    Hola Mundo - Clase 1

---
+++ Java/Leccion_1/HolaMundo.java
@@ -0,0 +1,13 @@
+//Primer HolaMundo con Java
+
+/*
+ * Linea 1
+ * Linea 2
+ * Linea 3
+ */
+
+public class HolaMundo {
+    public static void main(String args[]) {
+        System.out.println("Hola Mundo desde Java");
+    }
+}

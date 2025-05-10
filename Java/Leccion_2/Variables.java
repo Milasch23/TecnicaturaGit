f47d549182e65911545fc25de4cb2530b039f3e8
Milasch23 <kamyxsch@gmail.com>
2025-05-10 18:54:59 -0300

    Clase 2 Java

---
+++ Java/Leccion_2/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args){
+    int miVariable = 17;
+    System.out.println(miVariable);
+    miVariable = 8;
+    System.out.println(miVariable);
+    //Strings
+    String cadena = "Hola que tal";
+    System.out.println(cadena);
+    cadena = "Hello";
+    System.out.println(cadena);
+    }
+}

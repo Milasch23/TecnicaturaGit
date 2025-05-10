6b00ad3c69f6bcda0a1e250d495c16c970a413e7
Milasch23 <kamyxsch@gmail.com>
2025-05-10 15:45:02 -0300

    Cambios y Clase 4 real

--- Javascript/Leccion_3/03_03_tiposDeDatos.js
+++ Javascript/Leccion_3/03_03_tiposDeDatos.js
@@ -14,20 +14,3 @@ var objeto = {
 }
 console.log(typeof objeto); //Objeto
 
-var bandera = true;
-console.log(bandera);
-
-function miFuncion(){}
-console.log(typeof miFuncion);
-
-var simbolo = Symbol("Mi símbolo");
-console.log(typeof simbolo);
-
-class Persona {
-    constructor(nombre, apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
-    }
-}
-
-console.log(Persona);
---
+++ Javascript/Leccion_4/04_04_tiposDeDatos.js
@@ -0,0 +1,18 @@
+var bandera = true;
+console.log(bandera);
+
+function miFuncion(){}
+console.log(typeof miFuncion);
+
+var simbolo = Symbol("Mi símbolo");
+console.log(typeof simbolo);
+
+class Persona {
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+console.log(Persona);
+

fb62116aa18cce7fa9816250839bb464430dad5d
Milasch23 <kamyxsch@gmail.com>
2025-05-10 16:18:56 -0300

    Clase 5 Js

---
+++ Javascript/Leccion_5/05_05tiposDeDatos.js
@@ -0,0 +1,16 @@
+// Tipo de dato undefined
+var x;
+console.log(typeof(x));
+
+//null = ausencia de valor
+var y = null;
+console.log(typeof(y));
+
+// array y empty string
+var autos = ["Nissan", "Toyota", "Ford"];
+console.log(autos);
+console.log(typeof(autos));
+
+var z = "";
+console.log(z)
+console.log(typeof(z))

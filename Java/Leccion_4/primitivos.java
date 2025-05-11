7d78430137112fb87187b664b73cc9fbc0955f1f
Milasch23 <kamyxsch@gmail.com>
2025-05-10 22:07:43 -0300

    Clase 4 Java

---
+++ Java/Leccion_4/primitivos.java
@@ -0,0 +1,40 @@
+package Java.Leccion_4;
+
+public class primitivos {
+    public static void main(String[] args) {
+        byte numEnteroByte = (byte)149;
+        System.out.println(numEnteroByte);
+        System.out.println("Valor mínimo del byte: " + Byte.MIN_VALUE);
+        System.out.println("Valor máximo del byte: " + Byte.MAX_VALUE);
+
+        short numEnteroShort = 14;
+        System.out.println(numEnteroShort);
+        System.out.println("Valor mínimo del byte: " + Short.MIN_VALUE);
+        System.out.println("Valor máximo del byte: " + Short.MAX_VALUE);
+
+        int numEnteroInt = 25;
+        System.out.println(numEnteroInt);
+        System.out.println("Valor mínimo del byte: " + Integer.MIN_VALUE);
+        System.out.println("Valor máximo del byte: " + Integer.MAX_VALUE);
+
+        long numEnteroLong = 96;
+        System.out.println(numEnteroLong);
+        System.out.println("Valor mínimo del byte: " + Long.MIN_VALUE);
+        System.out.println("Valor máximo del byte: " + Long.MAX_VALUE);
+
+        float numFlotante = 10.56F;
+        System.out.println(numFlotante);
+        System.out.println("Valor mínimo del byte: " + Float.MIN_VALUE);
+        System.out.println("Valor máximo del byte: " + Float.MAX_VALUE);
+
+        double numDouble = 127;
+        System.out.println(numDouble);
+        System.out.println("Valor mínimo del byte: " + Double.MIN_VALUE);
+        System.out.println("Valor máximo del byte: " + Double.MAX_VALUE);
+
+        
+
+    }
+    
+
+}

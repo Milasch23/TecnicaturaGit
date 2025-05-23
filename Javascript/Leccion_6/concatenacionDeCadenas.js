2573c3c9806d365cf0896b072030f423a93c9772
Milasch23 <kamyxsch@gmail.com>
2025-05-23 18:18:21 -0300

    Clase 6 Js

---
+++ Javascript/Leccion_6/concatenacionDeCadenas.js
@@ -0,0 +1,16 @@
+var nombre = "José";
+var apellido = " Montés";
+var nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto)
+
+var nombreCompleto2 = "Camila " + "Scheurer";
+console.log(nombreCompleto2)
+
+var juntos = nombre + 478;
+console.log(juntos)
+
+juntos = 78 + 23 + nombre;
+console.log(juntos)
+
+nombre += apellido;
+console.log(nombre)

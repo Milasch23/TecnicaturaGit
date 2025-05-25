00db11a0169455fd06ac174a5937eff473dcf93c
Milasch23 <kamyxsch@gmail.com>
2025-05-25 16:09:35 -0300

    Clase 7 Javascript

---
+++ Javascript/Leccion_7/concatenacionDeCadenas2.js
@@ -0,0 +1,35 @@
+let nombre2 = "Lucas";
+console.log(nombre2);
+
+const apellido2 = "Martinez";
+//apeliido2 = "Navarro" <- X
+console.log(apellido2);
+
+let x, y;
+x = 13, y = 78;
+let z = x + y;
+console.log(z);
+
+let $1num = 18;
+
+let $break = "Hola";
+
+//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+//Ejercicio - Par o Impar
+
+let numero = 17;
+if (numero % 2 == 0){
+    console.log("Es par");
+}
+else {
+    console.log("Es impar");
+}
+
+//*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+let anio = 29;
+if (anio >= 18){
+    console.log("Es mayor de edad");
+}
+else {
+    console.log("No es mayor de edad.")
+}

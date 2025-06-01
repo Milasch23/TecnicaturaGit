995c00050610143e1eaf44d909fc937ba2f25fae
Milasch23 <kamyxsch@gmail.com>
2025-06-01 18:47:09 -0300

    JavaScript Clase 8

---
+++ Javascript/Leccion_8/operadorAndOr.js
@@ -0,0 +1,17 @@
+//Ejercicio - Dentro de un rango
+let dentroRango = 6;
+let min = 0, max = 10;
+
+if (dentroRango >= min && dentroRango <= max){
+    console.log("Está dentro del rango.")
+}
+else {
+    console.log("No está dentro del rango.")
+}
+
+let nombre = "Pedro";
+console.log(nombre);
+
+const apellido = "Tepes";
+console.log(apellido)
+
